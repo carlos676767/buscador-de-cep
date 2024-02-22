f5a7e4c27e53b16c8edd75f21f2068e20e9bcd77
carlos676767 <carlitobr730@gmail.com>
2024-02-21 16:30:27 -0800

    projeto finalizado

--- buscador/src/main.ts
+++ buscador/src/main.ts
@@ -1,27 +1,30 @@
 const buscarCep = () => {
-  let input = document.querySelector("input") as HTMLInputElement
-  const exibir = document.querySelector("p") as HTMLParagraphElement
-  fetch(`https://viacep.com.br/ws/${input.value}/json/`)
-  .then(response => response.json())
-  .then(data => {
- 
-  exibir.innerHTML = `
-  Bairro: ${data.bairro}<br>
-  Cep: ${data.cep}<br> 
-  Comprimento:  ${data.complemento} <br>
-  Ddd: ${data.ddd} <br>
-  Localidade: ${data.localidade} <br>
-  Uf: ${data.uf}`;
-  })
-  .catch(erro=> {
-    exibir.textContent = `Cep nao encontrado`
-    console.error("erro", erro)
-  })
-}
+  let input = document.querySelector("input") as HTMLInputElement;
+  const exibir = document.querySelector(".mensagem") as HTMLParagraphElement;
+  if (input.value === "") {
+    exibir.innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> campo vazio.`;
+  } else {
+    fetch(`https://viacep.com.br/ws/${input.value}/json/`)
+      .then((response) => response.json())
+      .then((data) => {
+        exibir.innerHTML = `
+  <i class="fa-solid fa-location-dot"></i> Bairro: ${data.bairro}<br>
+  <i class="fa-solid fa-location-crosshairs"></i> Cep: ${data.cep}<br> 
+  <i class="fa-solid fa-location-dot"></i> Comprimento: ${data.complemento} <br>
+  <i class="fa-solid fa-phone"></i> Ddd: ${data.ddd} <br>
+  <i class="fa-solid fa-location-dot"></i> Localidade: ${data.localidade} <br>
+  <i class="fa-solid fa-house"></i> UF: ${data.uf}`;
+      })
 
-const botao = document.querySelector("button") as HTMLButtonElement
+      .catch((erro) => {
+        exibir.innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> cep não encontrado.`;
+        console.error("erro", erro);
+      });
+  }
+};
+
+const botao = document.querySelector("button") as HTMLButtonElement;
 
 botao.addEventListener("click", () => {
-  buscarCep()
-})
-
+  buscarCep();
+});
