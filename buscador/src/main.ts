95eedbbbeeb0decae85762112183fc04b5625314
carlos676767 <carlitobr730@gmail.com>
2024-02-21 17:40:51 -0800

    regex adicionado

--- buscador/src/main.ts
+++ buscador/src/main.ts
@@ -1,19 +1,23 @@
 const buscarCep = () => {
   let input = document.querySelector("input") as HTMLInputElement;
   const exibir = document.querySelector(".mensagem") as HTMLParagraphElement;
+  const regex: RegExp = /^0{8}$/;
+
   if (input.value === "") {
     exibir.innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> campo vazio.`;
+  } else if (regex.test(input.value)) {
+    exibir.innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> entrada invalida.`;
   } else {
     fetch(`https://viacep.com.br/ws/${input.value}/json/`)
       .then((response) => response.json())
       .then((data) => {
         exibir.innerHTML = `
-  <i class="fa-solid fa-location-dot"></i> Bairro: ${data.bairro}<br>
-  <i class="fa-solid fa-location-crosshairs"></i> Cep: ${data.cep}<br> 
-  <i class="fa-solid fa-location-dot"></i> Comprimento: ${data.complemento} <br>
-  <i class="fa-solid fa-phone"></i> Ddd: ${data.ddd} <br>
-  <i class="fa-solid fa-location-dot"></i> Localidade: ${data.localidade} <br>
-  <i class="fa-solid fa-house"></i> UF: ${data.uf}`;
+<i class="fa-solid fa-location-dot"></i> Bairro: ${data.bairro}<br>
+<i class="fa-solid fa-location-crosshairs"></i> Cep: ${data.cep}<br> 
+<i class="fa-solid fa-location-dot"></i> Comprimento: ${data.complemento} <br>
+<i class="fa-solid fa-phone"></i> Ddd: ${data.ddd} <br>
+<i class="fa-solid fa-location-dot"></i> Localidade: ${data.localidade} <br>
+<i class="fa-solid fa-house"></i> UF: ${data.uf}`;
       })
 
       .catch((erro) => {
